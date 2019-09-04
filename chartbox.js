34adc18b3d1c20ae86c2d401522069131f3667fd
Brian McGill <brian.mcgill@wsj.com>
2019-09-04 18:07:59 -0400

    circle to rect, function ascatter added

--- chartbox.js
+++ chartbox.js
@@ -8,41 +8,51 @@ var dimB = parseInt(d3.select(".barbox").style("width")),
 var x = d3.scaleLinear().range([0, widthB]);
 var y = d3.scaleLinear().range([heightB, 0]);
 
-var svg = d3.select(".chart-popCng").append("svg")
-    .attr("width", widthB + margin.left + margin.right)
-    .attr("height", heightB + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
 
 d3.csv("data/acprural.csv", function(error, data) {
   if (error) throw error;
 
+
+function scatter(namez) {
    // format the data
   data.forEach(function(d) {
   	  d.color = d.color;
-      d.popCng = +d.popCng;
+      d[namez] = +d[namez];
   });
 
-  x.domain([d3.min(data, function(d) { return d.popCng; }), d3.max(data, function(d) { return d.popCng; })]);
+  var svg = d3.select(".chart-" + namez).append("svg")
+    .attr("width", widthB + margin.left + margin.right)
+    .attr("height", heightB + margin.top + margin.bottom)
+  	.append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+  x.domain([d3.min(data, function(d) { return d[namez]; }), d3.max(data, function(d) { return d[namez]; })]);
   y.domain(0);
 
   console.log(x.domain());
 
-  svg.selectAll("dot")
+  svg.selectAll("rect")
     .data(data)
-    .enter().append("circle")
+    .enter().append("rect")
     .attr("id", function(d) { return "county" + d.id; }) 
     .attr("fip", function(d) { return d.id; })
     .attr("r", 5)
-    .attr("cx", function(d) { return x(d.popCng); })
-    .attr("cy", 0)
+    .attr("x", function(d) { return x(d[namez]); })
+    .attr("y", 0)
+    .attr('height', '20px')
+    .attr('width', '2px')
     .style("fill", function(d) { return d.color })
+    
 
   // Add the X Axis
   svg.append("g")
-    .attr("transform", "translate(0,10)")
+    .attr("transform", "translate(0,30)")
     .call(d3.axisBottom(x));
 
+ };
+
+ scatter('popCng');
+ scatter('nhWhite');
 
 });
